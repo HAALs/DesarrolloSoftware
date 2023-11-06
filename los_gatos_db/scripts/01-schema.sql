4965a3da34d3348eb479ada9773b674e0adccc0d
CamiloVilchesB <142276138+CamiloVilchesB@users.noreply.github.com>
2023-11-06 20:19:37 -0300

    Insert Tabla Producto

--- los_gatos_db/scripts/01-schema.sql
+++ los_gatos_db/scripts/01-schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE tipo_producto (
 CREATE TABLE sub_tipo_producto (
   id_sub_tipo_producto bigint(11) NOT NULL AUTO_INCREMENT,
   nombre_sub_tipo_producto varchar(150) NOT NULL,
+  caracteristica varchar(150) NOT NULL,
   PRIMARY KEY (id_sub_tipo_producto),
   UNIQUE KEY nombre_sub_tipo_producto (nombre_sub_tipo_producto)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
