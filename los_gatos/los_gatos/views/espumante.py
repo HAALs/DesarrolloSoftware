36fc1cc566e62577f35f99f69fdca377353f0e32
HAALs <hal.almonacid.lopez@gmail.com>
2023-11-14 20:24:00 -0300

    imagenes en todas las paginas

--- los_gatos/los_gatos/views/espumante.py
+++ los_gatos/los_gatos/views/espumante.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from los_gatos.models.models import TipoProducto, Productos
 
 # Create your views here.
 def index(request):
-    return render(request, "espumantes.html")
+    print(f'request.method: {request.method}')
+    if request.method == 'GET': 
+        tipos = TipoProducto.objects.all()
+        products = Productos.objects.all()
+        return render(request, "espumantes.html", {"tipos": tipos, "products": products})
+    else: 
+        tipo = request.POST.get('tipo')
+        print(f'tipo: {tipo}')
+        tipos = TipoProducto.objects.all()
+        products = Productos.objects.filter(id_tipo_producto=tipo)
+        return render(request, "espumantes.html", {"tipos": tipos, "products": products})
