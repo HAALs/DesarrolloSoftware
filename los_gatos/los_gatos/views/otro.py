02e0cad989a06801a76e674e275b46096c0c9914
HAALs <hal.almonacid.lopez@gmail.com>
2023-11-17 16:16:09 -0300

    filtros arreglados

--- los_gatos/los_gatos/views/otro.py
+++ los_gatos/los_gatos/views/otro.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from los_gatos.models.models import TipoProducto, Productos
+from los_gatos.models.models import TipoProducto, Productos, SubTipoProducto
 
 # Create your views here.
 def index(request):
     print(f'request.method: {request.method}')
     if request.method == 'GET': 
-        tipos = TipoProducto.objects.filter(id_tipo_producto='5')
+        tipos = SubTipoProducto.objects.filter(id_sub_tipo_producto__in=[9,10])
         products = Productos.objects.filter(id_tipo_producto='5')
         return render(request, "otros.html", {"tipos": tipos, "products": products})
     else: 
         tipo = request.POST.get('tipo')
         print(f'tipo: {tipo}')
-        tipos = TipoProducto.objects.all()
+        tipos = SubTipoProducto.objects.filter(id_sub_tipo_producto__in=[9,10])
         products = Productos.objects.filter(id_tipo_producto=tipo)
         return render(request, "otros.html", {"tipos": tipos, "products": products})
