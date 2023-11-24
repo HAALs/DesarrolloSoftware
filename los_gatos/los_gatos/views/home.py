f19f3ae5ed16bf5f0c4644d8164cac274ddfc88d
HAALs <hal.almonacid.lopez@gmail.com>
2023-11-24 02:20:11 -0300

    no se

--- los_gatos/los_gatos/views/home.py
+++ los_gatos/los_gatos/views/home.py
@@ -1,16 +1,3 @@
-#from django.shortcuts import render
-#from django.http import HttpResponse
-#from los_gatos.models.models import Productos
-#
-## Create your views here.
-#def index(request):
-#    
-#    products = Productos.objects.all()
-#    print('products:', len(products))
-#    return render(request, "index.html", {'products': products})
-
-
-
 
 from django.shortcuts import render
 from django.http import HttpResponse
