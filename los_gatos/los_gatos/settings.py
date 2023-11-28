7f6014359cc5d7bc348bc30faf78ba193537a129
CamiloVilchesB <142276138+CamiloVilchesB@users.noreply.github.com>
2023-11-28 00:15:12 -0300

    Presentacion semifinal

--- los_gatos/los_gatos/settings.py
+++ los_gatos/los_gatos/settings.py
@@ -126,11 +126,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # settings.py
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'your_email_host'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@example.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'
+EMAIL_HOST_USER = 'botillerialosgatos2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'Losgatos2023'
 
 
 
